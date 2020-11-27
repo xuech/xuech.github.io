5dc16454048e6acb2f9f754cbed5024bb6ede89e
xuech <xuech_love@126.com>
2020-11-27 17:30:25 +0800

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   theme: 'reco',//博客主题
-  title: 'xuech\'s blog!!!!',
+  title: 'xuech\'s blog',
   description: '我的个人网站',
   head: [ // 注入到当前页面的 HTML <head> 中的标签
     ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
