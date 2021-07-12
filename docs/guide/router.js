0f9a36cb7bd93b38141f121671de90888bc608b2
xuech <xuech_love@126.com>
2021-07-12 10:53:38 +0800

    add code1

--- docs/guide/router.js
+++ docs/guide/router.js
@@ -1,49 +1,40 @@
 module.exports = [
-    {
-        title: "JavaScript基础",
-        collapsable: true,
-        children: [
-            '/guide/js/1',
-            '/guide/js/2',
-            '/guide/js/3',
-            '/guide/js/4',
-            '/guide/js/5',
-            '/guide/js/6',
-            '/guide/js/7',
-            '/guide/js/8'
-        ]
-    },
-    {
-        title: "CSS常见知识总结",
-        children: [
-            '/guide/css/1',
-            '/guide/css/2',
-            '/guide/css/3',
-            '/guide/css/4'
-        ]
-    },
-    {
-        title: "vue",
-        children: [
-            '/guide/vue/1',
-            '/guide/vue/2',
-            '/guide/vue/3',
-            '/guide/vue/4',
-            '/guide/vue/a',
-        ]
-    },
-    {
-        title: "webpack",
-        children: [
-            '/guide/webpack/1',
-            '/guide/webpack/2',
-            '/guide/webpack/3'
-        ]
-    },
-    // {
-    //     title: 'vue',
-    //     children: [
-    //         'guide/vues/1'
-    //     ]
-    // }
+  {
+    title: 'JavaScript基础',
+    collapsable: true,
+    children: [
+      '/guide/js/1',
+      '/guide/js/2',
+      '/guide/js/3',
+      '/guide/js/4',
+      '/guide/js/5',
+      '/guide/js/6',
+      '/guide/js/7',
+      '/guide/js/8',
+    ],
+  },
+  {
+    title: 'CSS常见知识总结',
+    children: ['/guide/css/1', '/guide/css/2', '/guide/css/3', '/guide/css/4'],
+  },
+  {
+    title: 'vue',
+    children: [
+      '/guide/vue/1',
+      '/guide/vue/2',
+      '/guide/vue/3',
+      '/guide/vue/4',
+      '/guide/vue/code1',
+    ],
+  },
+  {
+    title: 'webpack',
+    children: ['/guide/webpack/1', '/guide/webpack/2', '/guide/webpack/3'],
+  },
+  // {
+  //     title: 'vue',
+  //     children: [
+  //         'guide/vues/1'
+  //     ]
+  // }
 ]
